c2cb37926f7bfd5d43b595e79f0a2c5d595433ba
WeslleiBrito <wesllei.wbs@gmail.com>
2022-11-05 08:43:06 -0300

    Exercicio 2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,9 @@
-const num = //seu numero aqui
-
-if(){
-    console.log("sua mensagem aqui");
+function numeroEPar(numero) {
+    if (numero % 2 === 0) {
+        return `O número \"${numero}\" é Par`
+    }
 }
+
+const num = 566
+
+console.log(numeroEPar(num))
